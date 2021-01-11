b6106e9c4608a7a7a59d07ed8d9cd5b3b99442b5
Igor Efimov <efimov.036@gmail.com>
2021-01-12 04:07:51 +0700

    some renaming

--- app/src/main/java/com/delet_dis/bubbles/MainActivity.kt
+++ app/src/main/java/com/delet_dis/bubbles/MainActivity.kt
@@ -9,11 +9,11 @@ import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.delet_dis.bubbles.databinding.ActivityMainBinding
-import com.delet_dis.bubbles.view.MoBikeTagLayout
+import com.delet_dis.bubbles.view.BubblesLayout
 
 class MainActivity : AppCompatActivity() {
 
-  private lateinit var moBikeTagLayout: MoBikeTagLayout
+  private lateinit var bubblesLayout: BubblesLayout
   private lateinit var sensorManager: SensorManager
   private lateinit var accelerometerSensor: Sensor
   private lateinit var sensorEventListener: SensorEventListener
@@ -31,26 +31,26 @@ class MainActivity : AppCompatActivity() {
     sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
     accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 
-    moBikeTagLayout = binding.moBikeLayout
+    bubblesLayout = binding.bubblesLayout
 
     sensorEventListener = object : SensorEventListener {
       override fun onSensorChanged(event: SensorEvent) {
         if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
           val x = event.values[0]
           val y = event.values[1]
-          moBikeTagLayout.onSensorChanged(x, y)
+          bubblesLayout.onSensorChanged(x, y)
         }
       }
 
       override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
     }
 
-    moBikeTagLayout.setOnClickListener {
+    bubblesLayout.setOnClickListener {
       val randomSize = (50..300).random()
       val imageView = ImageView(applicationContext)
       imageView.setImageResource(R.drawable.bubble_vector)
       imageView.setColorFilter(Color.argb(255, (0..255).random(), (0..255).random(), (0..255).random()))
-      moBikeTagLayout.addView(imageView)
+      bubblesLayout.addView(imageView)
       imageView.layoutParams.height = randomSize
       imageView.layoutParams.width = randomSize
     }
